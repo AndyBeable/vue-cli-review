a2f47289c4f43f1f40cd40cec3bb19680b37c888
AndyBeable <andy.beable@gmail.com>
2022-01-08 15:29:52 +0000

    comp communication

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,26 @@
 <template>
-  <h1>Server Status: {{status}}</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-user></app-user>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import User from './components/User.vue';
+
 export default {
-  data () {
-    return {
-      status: 'Critical'
-    }
+  components: {
+    appUser: User
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+div.component {
+  border: 1px solid black;
+  padding: 30px;
 }
 </style>
